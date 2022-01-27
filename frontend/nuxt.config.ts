0e2e4eea5a497c8b6c151ccaf465d6132f367b91
smgcknt-tech <business.smgcknt@gmail.com>
2022-01-27 18:59:52 +0900

    changed config of vuetify

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,12 +2,9 @@ import { defineNuxtConfig } from "nuxt3"
 import confs from "./runtimeConfig"
 
 export default defineNuxtConfig({
-  css: ["vuetify/lib/styles/main.sass", "@/assets/css/main.scss"],
+  css: ["vuetify/lib/styles/main.sass", "@mdi/font/css/materialdesignicons.min.css"],
   publicRuntimeConfig: confs,
   build: {
-    parallel: true,
-    cache: true,
-    hardSource: true,
     transpile: ["vuetify"]
   }
 })
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,5 @@
 <template>
-  <div><v-btn depressed color="red">hello world</v-btn></div>
+  <div>
+    <MyComponent />
+  </div>
 </template>
